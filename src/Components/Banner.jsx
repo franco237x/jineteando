80353c5c7b4d79446843398023262301b50ad9a1
Gustavo Azzimonti Renzo <52264477+gazzimon@users.noreply.github.com>
2024-10-23 11:48:16 -0300

    Add files via upload

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,19 @@
+export default function Banner(){
+    return(
+        <>
+            <div className="banner container text-center">
+                <div className="row align-items-center">
+                    <main className="px-3 col">
+                        <h1>MISIO-BIKE♻️</h1>
+                        <p className="lead">¡Movilidad sostenible al alcance de un clic!</p>
+                        <p className="lead">
+                        <a href="#" className="btn btn-lg btn-light fw-bold border-white bg-white">Conocé más</a>
+                        </p>
+                    </main>
+    
+                </div>
+            </div>
+        </>
+    )
+
+}
---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom"
+export default function Card(props){
+    return(
+        <>
+        <div className="col-sm-6 mb-3 mb-sm-0">
+                <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.titulo}</h5>
+                    <p className="card-text">{props.texto}</p>
+                    <NavLink to={props.url} className="btn btn-primary">{props.boton}</NavLink>
+                </div>
+                </div>
+        </div>
+        </>
+    )
+}
